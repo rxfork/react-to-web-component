4ebd6657bee1c86dd4e4f8c3c3c165be44f356a4
Evan Sosenko <razorx@evansosenko.com>
2023-07-25 16:17:30 -0700

    Add callback type

--- packages/core/src/transforms/index.ts
+++ packages/core/src/transforms/index.ts
@@ -3,10 +3,11 @@ import number from "./number"
 import boolean from "./boolean"
 import function_ from "./function"
 import json from "./json"
+import callback from "./callback"
 
 export interface Transform<Type> {
   stringify?: (value: Type) => string
-  parse: (value: string, element: HTMLElement) => Type
+  parse?: (value: string, element: HTMLElement) => Type
 }
 
 const transforms = {
@@ -15,6 +16,7 @@ const transforms = {
   boolean,
   function: function_,
   json,
+  callback,
 }
 
 export type R2WCType = keyof typeof transforms
